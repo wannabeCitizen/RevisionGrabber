55ba60eb2cda190604074a0cfa7695331aede3bc
wannabeCitizen <doorsofskirpan@gmail.com>
2014-02-18 20:48:01 -0700

    Fixed indents in accordance with Python standards

--- parseDoc.py
+++ parseDoc.py
@@ -11,52 +11,52 @@ import sys
 import json
 
 def parseMe(myFile):
-	print "Creating a JSON object out of %s" % myFile
+    print "Creating a JSON object out of %s" % myFile
 
-	jsonRep = {}
+    jsonRep = {}
 
-	with open(myFile, "r") as pd:
-		currTeam = ""
-		for line in pd:
-			if re.match('Team', line):
-				currTeam = line.strip()
-				teamDict = parseTeam(pd)
-				jsonRep[currTeam] = teamDict
-			elif line == "\n":
-				continue
-			else:
-				newSegment = parseSegment(pd, line)
-				jsonRep[currTeam].update(newSegment)
+    with open(myFile, "r") as pd:
+        currTeam = ""
+        for line in pd:
+            if re.match('Team', line):
+                currTeam = line.strip()
+                teamDict = parseTeam(pd)
+                jsonRep[currTeam] = teamDict
+            elif line == "\n":
+                continue
+            else:
+                newSegment = parseSegment(pd, line)
+                jsonRep[currTeam].update(newSegment)
 
-	myJson = json.dumps(jsonRep)
-	print myJson
+    myJson = json.dumps(jsonRep)
+    print myJson
 
 def parseTeam(currFile):
-	team = {}
-	team['members'] = ""
-	nextLine = currFile.next()
-	while nextLine != "\n":
-		team['members'] += nextLine.strip()
-		nextLine = currFile.next()
-	return team
+    team = {}
+    team['members'] = ""
+    nextLine = currFile.next()
+    while nextLine != "\n":
+        team['members'] += nextLine.strip()
+        nextLine = currFile.next()
+    return team
 
 def parseSegment(currFile, thisLine):
-	newSeg = {}
-	myVal = thisLine.strip()
-	newSeg[myVal] = ""
-	nextLine = currFile.next()
-	while nextLine != "\n":
-		newSeg[myVal] += nextLine.strip()
-		try:
-			nextLine = currFile.next()
-		except:
-			print "End of File"
-			break
-	return newSeg
+    newSeg = {}
+    myVal = thisLine.strip()
+    newSeg[myVal] = ""
+    nextLine = currFile.next()
+    while nextLine != "\n":
+        newSeg[myVal] += nextLine.strip()
+        try:
+            nextLine = currFile.next()
+        except:
+            print "End of File"
+            break
+    return newSeg
 
-	
+    
 
 if __name__ == "__main__":
-	theFile = str(sys.argv[1])
-	parseMe(theFile)
+    theFile = str(sys.argv[1])
+    parseMe(theFile)
 
